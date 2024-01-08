d5c72dc2f4bdb218501d28547cbe6245ba29a8c4
Hoang Huong <huonghoangh7@gmail.com>
2024-01-08 16:15:18 +0700

    up lan 3

---
+++ js_nangcao-main/javascript/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,47 @@
+
+// `var`, `let`, & `const`
+
+// var và let sẽ thay thế được
+// const khong thay thế được trừ khi khai báo tham trị
+let firstName = 'John';
+const lastName = 'Doe';
+let age = 30;
+
+console.log(age);
+
+age = 31;
+console.log(age);
+
+let score;
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+
+console.log(score);
+
+const x = 100;
+
+
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: 'Brad',
+};
+person.name = 'John';
+person.email = 'brad@gmail.com';
+console.log(person);
+
+
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);
